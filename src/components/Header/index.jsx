e2b4d32fbd2b18f9c2f225f321df9c6cea058171
mkangoo <alexanderpavlov1408@gmail.com>
2024-05-16 23:56:00 +0300

    chore: added routing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,31 +1,34 @@
+import { Link } from 'react-router-dom'
 import style from './Header.module.scss'
 
 function Header({ onClickCart }) {
   return (
     <header className="d-flex justify-between align-center p-40">
-      <div className="d-flex align-center	">
-        <img width={40} height={40} src="/img/logo.png" alt="logo" />
-        <div>
-          <h3 className="text-uppercase">React Sneakers</h3>
-          <p className="opacity-5">Магазин лучших кроссовок</p>
+      <Link to="./">
+        <div className="d-flex align-center	">
+          <img width={40} height={40} src="/img/logo.png" alt="logo" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
         </div>
-      </div>
-      <nav>
-        <ul className="d-flex align-center">
-          <li onClick={onClickCart} className="mr-30 cu-p">
-            <img src="/img/icons/cart.svg" alt="Cart" />
-            <span>1205 р.</span>
-          </li>
-          <li className="mr-30 cu-p">
+      </Link>
+      <ul className="d-flex align-center">
+        <li onClick={onClickCart} className="mr-30 cu-p">
+          <img src="/img/icons/cart.svg" alt="Cart" />
+          <span>1205 р.</span>
+        </li>
+        <li className="mr-30 cu-p">
+          <Link to="./favorites">
             <img src="/img/icons/favorites-heart.svg" alt="Favorites" />
             <span>Закладки</span>
-          </li>
-          <li className="cu-p">
-            <img src="/img/icons/user.svg" alt="User" />
-            <span>Профиль</span>
-          </li>
-        </ul>
-      </nav>
+          </Link>
+        </li>
+        <li className="cu-p">
+          <img src="/img/icons/user.svg" alt="User" />
+          <span>Профиль</span>
+        </li>
+      </ul>
     </header>
   )
 }
