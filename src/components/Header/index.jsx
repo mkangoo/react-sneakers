6f86a3179186d658ef10f37c0888bfed59d0a5d8
mkangoo <alexanderpavlov1408@gmail.com>
2024-05-20 01:07:36 +0300

    chore: added a function for calculating the cart amount

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import AppContext from '../../context'
 import style from './Header.module.scss'
 
 function Header({ onClickCart }) {
+  const { cartItems } = useContext(AppContext)
+
+  const totalPrice = cartItems.reduce((sum, obj) => obj.price + sum, 0)
   return (
     <header className="d-flex justify-between align-center p-40">
       <Link to="./">
@@ -16,17 +21,19 @@ function Header({ onClickCart }) {
       <ul className="d-flex align-center">
         <li onClick={onClickCart} className="mr-30 cu-p">
           <img src="/img/icons/cart.svg" alt="Cart" />
-          <span>1205 р.</span>
+          <span>{totalPrice} руб.</span>
         </li>
         <li className="mr-30 cu-p">
-          <Link to="./favorites">
+          <Link to="/favorites">
             <img src="/img/icons/favorites-heart.svg" alt="Favorites" />
             <span>Закладки</span>
           </Link>
         </li>
         <li className="cu-p">
-          <img src="/img/icons/user.svg" alt="User" />
-          <span>Профиль</span>
+          <Link to="/orders">
+            <img src="/img/icons/user.svg" alt="User" />
+            <span>Профиль</span>
+          </Link>
         </li>
       </ul>
     </header>
