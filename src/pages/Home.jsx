01cb09df5ac55f7645f15520e88a006dead86f35
mkangoo <alexanderpavlov1408@gmail.com>
2024-05-21 22:22:41 +0300

    chore: added slide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,20 @@ function Home({ items, searchValue, setSearchValue, onChangeSearchInput, onAddTo
 
   return (
     <div className="content p-40">
+      <div className="slider slider-wrapper">
+        <div className="slider-content">
+          <div className="slider-content__info">
+            <img src="./img/adidas.png" width={100} height={40} alt="Adidas" />
+            <span>Stan Smith,</span>
+            <h3>Forever!</h3>
+            <button className="green-button ">Купить</button>
+          </div>
+        </div>
+        <div className="slider-frog__img">
+          <img src="./img/frog.png" width={642} height={300} alt="Frog" />
+        </div>
+      </div>
+      
       <div className="d-flex align-center justify-between mb-40">
         <h1>{searchValue ? `Поиск по запросу "${searchValue}"` : 'Все кроссовки'}</h1>
         <div className="search-block d-flex">
