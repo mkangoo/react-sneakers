8937f4cb81c7f251e553a94d82f6551d6b3b2aaf
mkangoo <alexanderpavlov1408@gmail.com>
2024-05-21 20:36:15 +0300

    chore: added logic for the Go back button in Favorites

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,19 +1,30 @@
 import React, { useContext } from 'react'
-import AppContext from '../context'
+import { Link, useLocation } from 'react-router-dom'
+import AppContext from '../Context/AppContext'
 
 const Info = ({ title, image, description }) => {
+  const { cartOpened } = useContext(AppContext)
   const { setCartOpened } = useContext(AppContext)
+  const location = useLocation()
   return (
     <>
       <div className="cartEmpty d-flex align-center text-center justify-center flex-column flex">
         <img className="mb-20" width={100} src={image} alt="Animation" />
         <h2>{title}</h2>
         <p className="opacity-6">{description}</p>
-
-        <button className="green-button" onClick={() => setCartOpened(false)}>
-          <img src="/img/icons/arrow.svg" alt="Arrow" />
-          <span>Вернуться назад</span>
-        </button>
+        {!cartOpened && location.pathname === '/favorites' ? (
+          <Link to="/">
+            <button className="green-button">
+              <img src="/img/icons/arrow.svg" alt="Arrow" />
+              <span>Вернуться назад</span>
+            </button>
+          </Link>
+        ) : (
+          <button className="green-button" onClick={() => setCartOpened(false)}>
+            <img src="/img/icons/arrow.svg" alt="Arrow" />
+            <span>Вернуться назад</span>
+          </button>
+        )}
       </div>
     </>
   )
